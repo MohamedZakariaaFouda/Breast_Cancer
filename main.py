e436e05bfb5f7caeab9b7d89c7997b6c1077c8d4
MohamedZakariaaFouda <mohamedzakariaafouda.com>
2026-06-21 07:41:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-print("🔥 MAIN STARTED")
+print("MAIN STARTED")
 
 import src
 import numpy as np
 from src.components.data_transformation import DataTransformation
 from src.components.model_trainer import ModelTrainer
 
-print("🔥 IMPORTS DONE")
+print("IMPORTS DONE")
 
 dt = DataTransformation()
 
-print("🔥 TRANSFORMATION OBJECT CREATED")
+print("TRANSFORMATION OBJECT CREATED")
 
 X_train_scaled, X_test_scaled, y_train, y_test, _ = dt.initiate_data_transformation(
     "artifacts/train.csv",
@@ -19,14 +19,14 @@ X_train_scaled, X_test_scaled, y_train, y_test, _ = dt.initiate_data_transformat
 train_arr = np.c_[X_train_scaled.values, y_train.values]
 test_arr = np.c_[X_test_scaled.values, y_test.values]
 
-print("🔥 DATA TRANSFORMATION DONE")
+print("DATA TRANSFORMATION DONE")
 
 mt = ModelTrainer()
 
-print("🔥 MODEL TRAINER CREATED")
+print("MODEL TRAINER CREATED")
 
 result = mt.initiate_model_training(train_arr, test_arr)
 
-print("🔥 MODEL TRAINING DONE")
+print("MODEL TRAINING DONE")
 
 print(result)
